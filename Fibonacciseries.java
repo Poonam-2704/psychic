30f31471e4cd8d9ede49efffecfd8844fb6beec5
Poonamsuthar <poonams2704@gmail.com>
2024-06-15 07:09:28 +0530

    FibonacciSeries.java

---
+++ Fibonacciseries.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Fibonacciseries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        int a = 0 , b =1, c;
+        System.out.print(a + "," + b + ",");
+        for(int i=0;i<n-2;i++){
+            c=a+b;
+            System.out.print(c + ",");
+            a=b;
+            b=c;
+        }
+    }
+    
+}
