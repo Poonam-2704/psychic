38c7ad5da7069f03ba4b1b39057d4096768553dc
Poonamsuthar <poonams2704@gmail.com>
2024-06-17 21:56:04 +0530

    Armstrong Number

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Armstrong{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int m=n;
+        int sum=0;
+        int r;
+        while(n>0){
+            r = n%10;
+            n=n/10;
+            sum=sum+r*r*r;
+        }
+        if(sum==m){
+            System.out.println("Armstrong number");
+        }
+        else{
+            System.out.println("Not an Armstrong number");
+        }
+    }
+}
